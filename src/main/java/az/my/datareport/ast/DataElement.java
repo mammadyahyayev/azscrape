210984fb09601c7117b10e19bcf377392495e487
MamedYahyayev <yahyayev2001@gmail.com>
2022-10-19 15:19:07 +0400

    Change config file structure

--- src/main/java/az/my/datareport/ast/DataElement.java
+++ src/main/java/az/my/datareport/ast/DataElement.java
@@ -2,15 +2,10 @@ package az.my.datareport.ast;
 
 public class DataElement {
     private final String name;
-    private final DataType type;
-    private final DataSelectorType selector_type;
     private final String selector;
 
-    public DataElement(String name, DataType type,
-                       DataSelectorType selector_type, String selector) {
+    public DataElement(String name, String selector) {
         this.name = name;
-        this.type = type;
-        this.selector_type = selector_type;
         this.selector = selector;
     }
 
@@ -18,15 +13,9 @@ public class DataElement {
         return name;
     }
 
-    public DataType getType() {
-        return type;
-    }
-
-    public DataSelectorType getSelector_type() {
-        return selector_type;
-    }
-
     public String getSelector() {
         return selector;
     }
+
+
 }
