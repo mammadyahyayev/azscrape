bc8a438816547b458e36909c483062932605c131
MamedYahyayev <yahyayev2001@gmail.com>
2022-11-23 19:58:29 +0400

    Change config file structure

--- src/main/java/az/my/datareport/parser/ConfigFile.java
+++ src/main/java/az/my/datareport/parser/ConfigFile.java
@@ -3,11 +3,13 @@ package az.my.datareport.parser;
 public class ConfigFile {
     private final String filename;
     private final String filepath;
+    private final String fileType;
     private final String fileExtension;
 
-    public ConfigFile(String filename, String filepath, String fileExtension) {
+    public ConfigFile(String filename, String filepath, String fileType, String fileExtension) {
         this.filename = filename;
         this.filepath = filepath;
+        this.fileType = fileType;
         this.fileExtension = fileExtension;
     }
 
@@ -21,5 +23,9 @@ public class ConfigFile {
 
     public String getFileExtension() {
         return fileExtension;
+    }
+
+    public String getFileType() {
+        return fileType;
     }
 }
