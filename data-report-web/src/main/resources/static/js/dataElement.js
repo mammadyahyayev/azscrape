b125f58180797820845bd2a044bae7ad98ceda33
MamedYahyayev <yahyayev2001@gmail.com>
2023-01-23 23:16:16 +0400

    Add id to data element

--- data-report-web/src/main/resources/static/js/dataElement.js
+++ data-report-web/src/main/resources/static/js/dataElement.js
@@ -1,4 +1,6 @@
 class DataElement {
+    id;
+
     constructor(name, selector) {
         this.name = name;
         this.selector = selector;
@@ -7,6 +9,14 @@ class DataElement {
     constructElement() {
         const elementBlock = document.createElement("div");
         elementBlock.className = "w-100 fs-4 mt-3 bg-light border border-2 p-3";
+
+        this.id = Math.ceil(Math.random() * 10000 + 1);
+        const idValue = document.createElement("p");
+        idValue.style.visibility = "hidden";
+        idValue.id = "element-id";
+        idValue.style.height = '0';
+        idValue.style.margin = '0';
+        idValue.textContent = this.id;
 
         const text = document.createElement("p");
         text.className = "m-0";
@@ -19,11 +29,12 @@ class DataElement {
         text.appendChild(removeBtn);
 
         elementBlock.appendChild(text);
+        elementBlock.appendChild(idValue);
 
         return elementBlock;
     }
 
     toString() {
-        return "{ " + this.name + ": [" + this.selector + "] }";
+        return "{ " + this.id + " - " + this.name + ": [" + this.selector + "] }";
     }
 }
