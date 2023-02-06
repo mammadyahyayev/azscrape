7733a627745429a746c42e0dccb789db172ad276
MamedYahyayev <yahyayev2001@gmail.com>
2023-02-06 14:53:33 +0400

    Move FileUtility methods into FileManager class

--- data-report-core/src/main/java/az/my/datareport/utils/FileManager.java
+++ data-report-core/src/main/java/az/my/datareport/utils/FileManager.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 public class FileManager {
     private static final Logger LOG = LogManager.getLogger(FileManager.class);
 
+    private static final char FILE_NAME_DELIMITER = '_';
+
     public FileManager() {
     }
 
@@ -72,6 +74,31 @@ public class FileManager {
         }
 
         return file;
+    }
+
+    public static String constructFilename(String name) {
+        String filename = name.toLowerCase();
+        return StringUtils.replaceAllSymbols(filename, FILE_NAME_DELIMITER);
+    }
+
+    public static String constructFilename(String name, String extension) {
+        Assert.required(name, "name is required field");
+        Assert.required(extension, "extension is required field");
+
+        String filename = name.toLowerCase();
+        filename = StringUtils.replaceAllSymbols(filename, FILE_NAME_DELIMITER);
+        return filename + "." + extension;
+    }
+
+    public static File getFile(String filepath) {
+        Assert.required(filepath, "filepath is required to construct File object");
+
+        File file = new File(filepath);
+        if (file.exists() && file.canRead()) {
+            return file;
+        }
+
+        return null;
     }
 
 }
