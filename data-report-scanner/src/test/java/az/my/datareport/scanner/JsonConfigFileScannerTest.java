9e0749ff1c2b5ecfbef2053c7cf92b61eedafe69
MamedYahyayev <yahyayev2001@gmail.com>
2023-02-07 17:46:40 +0400

    Fix failed tests, close #10

--- data-report-scanner/src/test/java/az/my/datareport/scanner/JsonConfigFileScannerTest.java
+++ data-report-scanner/src/test/java/az/my/datareport/scanner/JsonConfigFileScannerTest.java
@@ -2,19 +2,31 @@ package az.my.datareport.scanner;
 
 import az.my.datareport.config.ConfigFile;
 import az.my.datareport.config.ConfigFileException;
-import az.my.datareport.constant.TestConstants;
 import az.my.datareport.model.ReportFile;
 import az.my.datareport.tree.DataAST;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.net.URL;
 
-import static az.my.datareport.constant.TestConstants.CONFIG_FILE_PATH;
-import static az.my.datareport.constant.TestConstants.EMPTY_CONFIG_FILE_PATH;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JsonConfigFileScannerTest {
+
+    static String configFilePath = "";
+
+    @BeforeAll
+    static void setUp() {
+        ClassLoader classLoader = JsonConfigFileScannerTest.class.getClassLoader();
+        URL resource = classLoader.getResource("config.json");
+        if (resource == null) {
+            fail("config.json was not found");
+        }
+
+        configFilePath = resource.getPath();
+    }
 
     @Test
     void testReadDataConfig_whenPathInvalid_throwException() {
@@ -25,21 +37,30 @@ class JsonConfigFileScannerTest {
 
     @Test
     void testReadDataConfig_whenFileIsEmpty_throwException() {
+        //given
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource("test-resources/empty-config.json");
+        if (resource == null) {
+            fail("empty-config.json was not found");
+        }
+
+        String path = resource.getPath();
         ConfigFileScanner scanner = new JsonConfigFileScanner();
-        assertThrows(ConfigFileException.class, () -> scanner.readDataConfig(EMPTY_CONFIG_FILE_PATH.toString()));
+        assertThrows(ConfigFileException.class, () -> scanner.readDataConfig(path));
     }
 
     @Test
     void testReadDataConfig_whenTrueFileGiven_returnParsedDataAsAST() {
         ConfigFileScanner scanner = new JsonConfigFileScanner();
-        DataAST actual = scanner.readDataConfig(TestConstants.CONFIG_FILE_PATH.toString());
+        DataAST actual = scanner.readDataConfig(configFilePath);
         assertNotNull(actual);
     }
 
     @DisplayName("it will read report file configurations in given config file")
     @Test
     void testReadFileConfig_whenConfigFileGiven_thenReturnReportFile() {
-        ConfigFile configFile = new ConfigFile("config.json", CONFIG_FILE_PATH.toString(), "json");
+        ConfigFile configFile = new ConfigFile("config.json", configFilePath, "json");
+
         ConfigFileScanner scanner = new JsonConfigFileScanner();
         ReportFile reportFile = scanner.readFileConfig(configFile);
         assertNotNull(reportFile);
--- data-report-scanner/src/test/java/az/my/datareport/scanner/JsonSyntaxNormalizerTest.java
+++ data-report-scanner/src/test/java/az/my/datareport/scanner/JsonSyntaxNormalizerTest.java
@@ -1,6 +1,5 @@
 package az.my.datareport.scanner;
 
-import az.my.datareport.constant.TestConstants;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -8,17 +7,24 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class JsonSyntaxNormalizerTest {
 
     @Test
     void testNormalize_whenAbnormalKeysGiven_returnNormalizeFormOfThem() {
+        //given
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource("test-resources/test-config.json");
+        if (resource == null) {
+            fail("empty-config.json was not found");
+        }
+
         ObjectMapper mapper = new ObjectMapper();
         try {
-            JsonNode jsonNode = mapper.readTree(new File(TestConstants.TEST_CONFIG_FILE_PATH.toString()));
+            JsonNode jsonNode = mapper.readTree(new File(resource.getPath()));
             ObjectNode node = JsonSyntaxNormalizer.normalize(jsonNode);
             assertNull(node.get("TitLe"));
             assertNotNull(node.get("title"));
