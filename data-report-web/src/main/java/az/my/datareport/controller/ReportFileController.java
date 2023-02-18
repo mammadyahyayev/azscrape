facae19f606cf3cf937351a13d36c235e41ac85c
MamedYahyayev <yahyayev2001@gmail.com>
2023-02-18 21:21:41 +0400

    Customize Save as window, close #14

--- data-report-web/src/main/java/az/my/datareport/controller/ReportFileController.java
+++ data-report-web/src/main/java/az/my/datareport/controller/ReportFileController.java
@@ -12,12 +12,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -62,13 +64,15 @@ public class ReportFileController {
     }
 
     @GetMapping(value = "/download", produces = "application/octet-stream")
-    public ResponseEntity<Resource> downloadReportFile() {
+    public ResponseEntity<Resource> downloadReportFile(HttpServletResponse response) {
         ReportFile reportFile = configService.getReportFileConfiguration();
         Path reportFilePath = Path.of(FileConstants.TEMP_DIR_PATH, reportFile.getFileFullName());
 
         try {
             InputStream inputStream = new FileInputStream(String.valueOf(reportFilePath.toAbsolutePath()));
             InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+
+            response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + reportFile.getFileFullName());
 
             return ResponseEntity.ok()
                     .contentType(MediaType.APPLICATION_OCTET_STREAM)
