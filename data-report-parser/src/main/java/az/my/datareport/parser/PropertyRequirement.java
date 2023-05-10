c8e7c74b298f5ab157c09ee3dcb2b84bc07e3dd4
MamedYahyayev <yahyayev2001@gmail.com>
2023-05-10 18:49:24 +0400

    Remove data-report-parser module

--- data-report-parser/src/main/java/az/my/datareport/parser/PropertyRequirement.java
+++
@@ -1,5 +0,0 @@
-package az.my.datareport.parser;
-
-public enum PropertyRequirement {
-    REQUIRED, OPTIONAL
-}
--- data-report-parser/src/main/java/az/my/datareport/parser/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Read and parsed config file data
- *
- * @author Mammad Yahyayev
- * @since 1.0.0
- */
-package az.my.datareport.parser;
