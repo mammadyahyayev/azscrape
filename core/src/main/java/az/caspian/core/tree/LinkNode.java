162cebfe399791791a8760c2a8c84346cf213340
MamedYahyayev <yahyayev2001@gmail.com>
2023-09-16 21:16:12 +0400

    Add LinkNode and ListNode

---
+++ core/src/main/java/az/caspian/core/tree/LinkNode.java
@@ -0,0 +1,8 @@
+package az.caspian.core.tree;
+
+public class LinkNode extends Node {
+
+    public LinkNode(String link, String selector) {
+        super(link, selector);
+    }
+}
---
+++ core/src/main/java/az/caspian/core/tree/ListNode.java
@@ -0,0 +1,7 @@
+package az.caspian.core.tree;
+
+public class ListNode extends ParentNode {
+    public ListNode(String name, String selector) {
+        super(name, selector);
+    }
+}
