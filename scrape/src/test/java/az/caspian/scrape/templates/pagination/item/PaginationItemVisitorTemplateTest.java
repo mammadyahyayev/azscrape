92466640e7f27bce259d3b5fbfbbf4c9f20c422e
MamedYahyayev <yahyayev2001@gmail.com>
2023-11-12 16:39:43 +0400

    Fix failing test

--- scrape/src/test/java/az/caspian/scrape/templates/pagination/item/PaginationItemVisitorTemplateTest.java
+++ scrape/src/test/java/az/caspian/scrape/templates/pagination/item/PaginationItemVisitorTemplateTest.java
@@ -3,6 +3,7 @@ package az.caspian.scrape.templates.pagination.item;
 import static az.caspian.scrape.templates.pagination.PageParameters.PAGE_SPECIFIER;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import az.caspian.core.tree.DataNode;
 import az.caspian.core.tree.DataTree;
 import az.caspian.scrape.templates.pagination.PageParameters;
 import org.junit.jupiter.api.Test;
@@ -14,8 +15,10 @@ class PaginationItemVisitorTemplateTest {
     var pageParameters =
         new PageParameters.Builder().url("https://turbo.az/autos?page=" + PAGE_SPECIFIER).build();
 
+    var tree = new DataTree<>();
+    tree.addNode(new DataNode("", ""));
+
     assertThrows(
-        IllegalStateException.class,
-        () -> new PaginationItemVisitorTemplate(pageParameters, new DataTree<>()));
+        IllegalStateException.class, () -> new PaginationItemVisitorTemplate(pageParameters, tree));
   }
 }
