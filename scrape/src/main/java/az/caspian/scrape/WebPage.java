aeac6e03064c631a3bb2b023fb80758fa12b0847
MamedYahyayev <yahyayev2001@gmail.com>
2023-11-12 16:22:43 +0400

    Refactor code

--- scrape/src/main/java/az/caspian/scrape/WebPage.java
+++ scrape/src/main/java/az/caspian/scrape/WebPage.java
@@ -1,153 +1,151 @@
 package az.caspian.scrape;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
+import static java.lang.String.format;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 
-import static java.lang.String.format;
-
-/**
- * A Web Page
- */
+/** A Web Page */
 public class WebPage {
-    private static final Logger LOG = LogManager.getLogger(WebPage.class);
+  private static final Logger LOG = LogManager.getLogger(WebPage.class);
 
-    private final String url;
-    private final boolean isConnected;
+  private final String url;
+  private final boolean isConnected;
 
-    WebPage(String url) {
-        Objects.requireNonNull(url, "Webpage url cannot be null!");
-        //TODO: Check given url is valid
-        this.url = url;
-        this.isConnected = true;
+  WebPage(String url) {
+    Objects.requireNonNull(url, "Webpage url cannot be null!");
+    // TODO: Check given url is valid
+    this.url = url;
+    this.isConnected = true;
+  }
+
+  /**
+   * Fetch web elements and stores them in list of strings
+   *
+   * @param cssSelector selector of the element
+   * @return scraped elements
+   */
+  public String fetchElementsAsText(String cssSelector, WebElement webElement) {
+    String text;
+    try {
+      text =
+          webElement.findElements(By.cssSelector(cssSelector)).stream()
+              .map(WebElement::getText)
+              .collect(Collectors.joining("\n"));
+    } catch (Exception e) {
+      LOG.error("Unknown error happened");
+      text = "";
     }
 
+    return text;
+  }
 
-    /**
-     * Fetch web elements and stores them in list of strings
-     *
-     * @param cssSelector selector of the element
-     * @return scraped elements
-     */
-    public String fetchElementsAsText(String cssSelector, WebElement webElement) {
-        String text;
-        try {
-            text = webElement.findElements(By.cssSelector(cssSelector))
-                    .stream()
-                    .map(WebElement::getText).collect(Collectors.joining("\n"));
-        } catch (Exception e) {
-            LOG.error("Unknown error happened");
-            text = "";
-        }
-
-        return text;
+  /**
+   * Fetch a web element
+   *
+   * @param cssSelector selector of the element
+   * @return scraped element
+   */
+  public String fetchElementAsText(String cssSelector, WebElement webElement) {
+    if (webElement == null) {
+      return "";
     }
 
-    /**
-     * Fetch a web element
-     *
-     * @param cssSelector selector of the element
-     * @return scraped element
-     */
-    public String fetchElementAsText(String cssSelector, WebElement webElement) {
-        if (webElement == null) {
-            return "";
-        }
-
-        String text = "";
-        try {
-            WebElement element = webElement.findElement(By.cssSelector(cssSelector));
-            if (element != null)
-                text = element.getText();
-        } catch (Exception e) {
-            LOG.error("Unknown error happened: " + e);
-            text = "";
-        }
-
-        return text;
+    String text = "";
+    try {
+      WebElement element = webElement.findElement(By.cssSelector(cssSelector));
+      if (element != null) text = element.getText();
+    } catch (Exception e) {
+      LOG.error("Unknown error happened: " + e);
+      text = "";
     }
 
-    public String fetchElementAsText(String cssSelector) {
-        String text;
-        try {
-            text = WebBrowser.DRIVER.findElement(By.cssSelector(cssSelector)).getText();
-        } catch (Exception e) {
-            LOG.error("Unknown error happened: " + e);
-            text = "";
-        }
+    return text;
+  }
 
-        return text;
+  public String fetchElementAsText(String cssSelector) {
+    String text;
+    try {
+      text = WebBrowser.DRIVER.findElement(By.cssSelector(cssSelector)).getText();
+    } catch (Exception e) {
+      LOG.error("Unknown error happened: " + e);
+      text = "";
     }
 
-    /**
-     * Fetch web elements and stores them in list of strings
-     *
-     * @param cssSelector selector of the element
-     * @return scraped elements as WebElement
-     * @see WebElement
-     */
-    public List<WebElement> fetchWebElements(String cssSelector) {
-        List<WebElement> elements = new ArrayList<>();
-        try {
-            elements = new ArrayList<>(WebBrowser.DRIVER.findElements(By.cssSelector(cssSelector)));
-        } catch (Exception e) {
-            LOG.error("Unknown error happened: " + e);
-        }
+    return text;
+  }
 
-        return elements;
+  /**
+   * Fetch web elements and stores them in list of strings
+   *
+   * @param cssSelector selector of the element
+   * @return scraped elements as WebElement
+   * @see WebElement
+   */
+  public List<WebElement> fetchWebElements(String cssSelector) {
+    List<WebElement> elements = new ArrayList<>();
+    try {
+      elements = new ArrayList<>(WebBrowser.DRIVER.findElements(By.cssSelector(cssSelector)));
+    } catch (Exception e) {
+      LOG.error("Unknown error happened: " + e);
     }
 
-    public Optional<WebElement> fetchWebElement(String cssSelector) {
-        WebElement element;
-        try {
-            element = WebBrowser.DRIVER.findElement(By.cssSelector(cssSelector));
-            return Optional.of(element);
-        } catch (Exception e) {
-            LOG.error("Unknown error happened: " + e);
-        }
+    return elements;
+  }
 
-        return Optional.empty();
+  public Optional<WebElement> fetchWebElement(String cssSelector) {
+    WebElement element;
+    try {
+      element = WebBrowser.DRIVER.findElement(By.cssSelector(cssSelector));
+      return Optional.of(element);
+    } catch (Exception e) {
+      LOG.error("Unknown error happened: " + e);
     }
 
-    /**
-     * Scrolls to the end of Web Page
-     */
-    public void scrollToEnd() {
-        JavascriptExecutor js = (JavascriptExecutor) WebBrowser.DRIVER;
-        long amount = (long) js.executeScript("return document.body.offsetHeight");
+    return Optional.empty();
+  }
 
-        js.executeScript(format("window.scrollBy(0, %d)", amount));
-    }
+  /** Scrolls to the end of Web Page */
+  public void scrollToEnd() {
+    JavascriptExecutor js = (JavascriptExecutor) WebBrowser.DRIVER;
+    long amount = (long) js.executeScript("return document.body.offsetHeight");
 
-    /**
-     * Returns height of the Web Page scroll from beginning
-     * to end.
-     *
-     * @return scroll height
-     */
-    public long height() {
-        JavascriptExecutor js = (JavascriptExecutor) WebBrowser.DRIVER;
-        return (long) js.executeScript("return document.body.scrollHeight");
-    }
+    js.executeScript(format("window.scrollBy(0, %d)", amount));
+  }
 
-    /**
-     * Checks whether web page opened in WebDriver, or not
-     *
-     * @return {@code true}, if web page opened in WebDriver, otherwise {@code false}
-     */
-    public boolean isConnected() {
-        return isConnected;
-    }
+  public void scroll(int scrollHeight) {
+    JavascriptExecutor js = (JavascriptExecutor) WebBrowser.DRIVER;
+    js.executeScript(format("window.scrollBy(0, %d)", scrollHeight));
+  }
 
-    public String getUrl() {
-        return url;
-    }
+  /**
+   * Returns height of the Web Page scroll from beginning to end.
+   *
+   * @return scroll height
+   */
+  public long height() {
+    JavascriptExecutor js = (JavascriptExecutor) WebBrowser.DRIVER;
+    return (long) js.executeScript("return document.body.scrollHeight");
+  }
+
+  /**
+   * Checks whether web page opened in WebDriver, or not
+   *
+   * @return {@code true}, if web page opened in WebDriver, otherwise {@code false}
+   */
+  public boolean isConnected() {
+    return isConnected;
+  }
+
+  public String getUrl() {
+    return url;
+  }
 }
