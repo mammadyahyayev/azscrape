216829e7a52c57116dea0fc2df64dc6e6ccce076
MamedYahyayev <yahyayev2001@gmail.com>
2023-12-14 12:33:23 +0400

    Create FileType

--- core/src/main/java/az/caspian/core/model/enumeration/FileExtension.java
+++ core/src/main/java/az/caspian/core/model/enumeration/FileExtension.java
@@ -1,19 +1,20 @@
 package az.caspian.core.model.enumeration;
 
 public enum FileExtension {
-    XLSX, TXT, JSON;
+  XLSX,
+  CSV;
 
-    public static boolean contains(String extension) {
-        if (extension == null || extension.isEmpty()) {
-            return false;
-        }
-
-        for (FileExtension fileExtension : values()) {
-            if (fileExtension.name().equals(extension.toUpperCase())) {
-                return true;
-            }
-        }
-
-        return false;
+  public static boolean contains(String extension) {
+    if (extension == null || extension.isEmpty()) {
+      return false;
     }
+
+    for (FileExtension fileExtension : values()) {
+      if (fileExtension.name().equals(extension.toUpperCase())) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
--- core/src/main/java/az/caspian/core/model/enumeration/FileType.java
+++ core/src/main/java/az/caspian/core/model/enumeration/FileType.java
@@ -1,19 +1,20 @@
 package az.caspian.core.model.enumeration;
 
 public enum FileType {
-    CSV, TEXT, JSON, EXCEL;
+  CSV,
+  EXCEL;
 
-    public static boolean contains(String type) {
-        if (type == null || type.isEmpty()) {
-            return false;
-        }
-
-        for (FileType fileType : values()) {
-            if (fileType.name().equals(type.toUpperCase())) {
-                return true;
-            }
-        }
-
-        return false;
+  public static boolean contains(String type) {
+    if (type == null || type.isEmpty()) {
+      return false;
     }
+
+    for (FileType fileType : values()) {
+      if (fileType.name().equals(type.toUpperCase())) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
