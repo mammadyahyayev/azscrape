52f31363fe4d28951abb2561105815bf8bd66d66
MamedYahyayev <yahyayev2001@gmail.com>
2023-12-21 17:04:28 +0400

    Create a JoinToProjectFrame

---
+++ client/src/main/java/az/caspian/client/JoinToProjectFrame.java
@@ -0,0 +1,15 @@
+package az.caspian.client;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JoinToProjectFrame {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("AZScrape Client");
+    frame.setVisible(true);
+    frame.setBackground(Color.CYAN);
+    frame.setSize(420, 420);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setLocation(700, 450);
+  }
+}
