0a0e41db13b0130d2a756bea7099d0d23009a987
MamedYahyayev <yahyayev2001@gmail.com>
2023-12-28 06:33:50 +0400

    Create app folder inside user.home

--- client/src/main/java/az/caspian/client/Main.java
+++ client/src/main/java/az/caspian/client/Main.java
@@ -1,9 +1,30 @@
 package az.caspian.client;
 
 import az.caspian.client.ui.JoinToProjectFrame;
+import az.caspian.core.AzScrapeAppException;
+import az.caspian.core.constant.FileConstants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class Main {
-  public static void main(String[] args)   {
+  private static final Logger LOG = LogManager.getLogger(Main.class);
+
+  public static void main(String[] args) {
+    LOG.info("AZScrape client is about to start...");
+
+    try {
+      if (!Files.exists(FileConstants.APP_PATH)) {
+        Files.createDirectory(FileConstants.APP_PATH);
+        LOG.info("Main app folder is created...");
+      }
+    } catch (IOException e) {
+      LOG.error("Failed to create main application folder!");
+      throw new AzScrapeAppException("Failed to create main app folder", e);
+    }
+
     new JoinToProjectFrame();
   }
 }
--- core/src/main/java/az/caspian/core/constant/FileConstants.java
+++ core/src/main/java/az/caspian/core/constant/FileConstants.java
@@ -5,21 +5,22 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileConstants {
-    public static final String TEMP_DIR_PATH = getTempDirPath();
+  public static final Path APP_PATH = Path.of(System.getProperty("user.home"), ".azscrape");
+  public static final String TEMP_DIR_PATH = getTempDirPath();
 
-    public static final String USER_DIR = System.getProperty("user.dir");
-    public static final String MAIN_RESOURCES = Path.of("src", "main", "resources").toString();
-    public static final String TEST_RESOURCES = Path.of(USER_DIR, "src", "test", "resources").toString();
-    public static final String CONFIG_FILE_PATH = Path.of(MAIN_RESOURCES, "config.json").toString();
+  public static final String USER_DIR = System.getProperty("user.dir");
+  public static final String MAIN_RESOURCES = Path.of("src", "main", "resources").toString();
+  public static final String TEST_RESOURCES = Path.of(USER_DIR, "src", "test", "resources").toString();
+  public static final String CONFIG_FILE_PATH = Path.of(MAIN_RESOURCES, "config.json").toString();
 
-    // MODULE Paths
-    private static String getTempDirPath() {
-        try {
-            Path tempDir = Files.createTempDirectory("__exported_files__");
-            return tempDir.toString();
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to create temp directory: " + e.getMessage());
-        }
+  // MODULE Paths
+  private static String getTempDirPath() {
+    try {
+      Path tempDir = Files.createTempDirectory("__exported_files__");
+      return tempDir.toString();
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to create temp directory: " + e.getMessage());
     }
+  }
 
 }
