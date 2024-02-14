35f3f5ab61c031c143f3e0293faf7958b9e7debf
MamedYahyayev <yahyayev2001@gmail.com>
2024-02-14 14:38:47 +0400

    Rename Master and Slave ClientTypes to Coordinator and Worker respectively

--- core/src/main/java/az/caspian/core/messaging/ClientInfo.java
+++ core/src/main/java/az/caspian/core/messaging/ClientInfo.java
@@ -3,8 +3,26 @@ package az.caspian.core.messaging;
 import java.io.Serializable;
 import java.util.Map;
 
+/**
+ * The type of client which assigned to identify his role on projects.
+ */
 enum ClientType {
-  MASTER, SLAVE
+  /**
+   * The project owner or the client who shares his project publicly
+   * for other {@link #WORKER} Clients can join.
+   * <b>Note:</b> {@link #WORKER} type will be a default type for Clients.
+   * However, if a Client shares his project publicly, then it will become
+   * {@link #COORDINATOR} and maintainer of his project.
+   */
+  COORDINATOR,
+
+  /**
+   * This is the default Client type.
+   * The type of client who will do whatever jobs assigned to him
+   * and will be in sync or async communication with {@link #COORDINATOR}
+   * to report status of the job, to send collected data and so on.
+   */
+  WORKER
 }
 
 public final class ClientInfo implements Serializable {
@@ -14,7 +32,7 @@ public final class ClientInfo implements Serializable {
   private String email;
   private String ipAddress;
   private Map<String, String> computerDetails;
-  private ClientType clientType = ClientType.SLAVE;
+  private ClientType clientType = ClientType.WORKER;
 
   public String getFullName() {
     return this.firstName + " " + this.lastName;
