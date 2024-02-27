ea49d5fe54681458036e3a10c61b12b5b7c10133
MamedYahyayev <yahyayev2001@gmail.com>
2024-02-27 15:56:38 +0400

    Refactor PageParameters

--- scrape/src/main/java/az/caspian/scrape/templates/pagination/PageParameters.java
+++ scrape/src/main/java/az/caspian/scrape/templates/pagination/PageParameters.java
@@ -60,18 +60,18 @@ public class PageParameters {
         }
 
         if (pageSpecifier.type == PageSpecifierType.QUERY_PARAM) {
-            if (queryParameters.size() > 0)
+            if (!queryParameters.isEmpty())
                 url += QUERY_PARAM_SEPARATOR + queryParams;
 
         } else if (pageSpecifier.type == PageSpecifierType.HASH_FRAGMENT) {
             String fragmentPart = url.substring(pageSpecifier.begin, pageSpecifier.end + 1);
 
-            if (queryParameters.size() > 0) {
+            if (!queryParameters.isEmpty()) {
                 String queryParamsPart = url.substring(0, pageSpecifier.begin) + "?" + queryParams;
                 url = queryParamsPart + fragmentPart;
             }
         } else {
-            if (queryParameters.size() > 0)
+            if (!queryParameters.isEmpty())
                 url += "?" + queryParams;
         }
 
@@ -153,61 +153,48 @@ public class PageParameters {
         NOT_SET, QUERY_PARAM, URL_PATH, HASH_FRAGMENT
     }
 
-    private static class PageSpecifier {
-        private final PageSpecifierType type;
-        private final int begin;
-        private final int end;
+  private record PageSpecifier(PageSpecifierType type, int begin, int end) {
+  }
 
-        PageSpecifier(PageSpecifierType type, int begin, int end) {
-            this.type = type;
-            this.begin = begin;
-            this.end = end;
-        }
+  private record PageSpecifierDetection(String url) {
+    private static final String PAGE_SPECIFIER_KEY = "pageNum";
+
+    private static final char HASH_FRAGMENT_SPECIFIER = '#';
+    private static final char QUERY_PARAM_SPECIFIER = '?';
+
+    private PageSpecifierDetection(String url) {
+      this.url = Objects.requireNonNull(url);
     }
 
-    private static class PageSpecifierDetection {
-        private static final String PAGE_SPECIFIER_KEY = "pageNum";
+    public PageSpecifier detect() {
+      int begin = url.indexOf('{');
+      int end = url.indexOf('}');
 
-        private static final char HASH_FRAGMENT_SPECIFIER = '#';
-        private static final char QUERY_PARAM_SPECIFIER = '?';
+      if (begin == -1 || end == -1 || begin > end) {
+        throw new IllegalArgumentException(
+          format("Page specifier isn't configured correctly!, begin=%d, end=%d", begin, end)
+        );
+      }
 
-        private final String url;
+      int pageSpecifierKey = url.indexOf(PAGE_SPECIFIER_KEY);
+      if (pageSpecifierKey == -1 || pageSpecifierKey < begin || pageSpecifierKey > end) {
+        throw new IllegalArgumentException(format("Page specifier is in wrong place!, begin=%d, pageKey=%d, end=%d",
+            begin, pageSpecifierKey, end));
+      }
 
-        PageSpecifierDetection(String url) {
-            this.url = Objects.requireNonNull(url);
-        }
+      int hashFragmentSpecIndex = url.indexOf(HASH_FRAGMENT_SPECIFIER);
+      if (hashFragmentSpecIndex != -1) {
+        return new PageSpecifier(PageSpecifierType.HASH_FRAGMENT, hashFragmentSpecIndex, end);
+      }
 
-        public PageSpecifier detect() {
-            int begin = url.indexOf('{');
-            int end = url.indexOf('}');
+      int queryParamSpecIndex = url.indexOf(QUERY_PARAM_SPECIFIER);
+      if (queryParamSpecIndex != -1) {
+        return new PageSpecifier(PageSpecifierType.QUERY_PARAM, queryParamSpecIndex, end);
+      }
 
-            if (begin == -1 || end == -1 || begin > end) {
-                throw new IllegalArgumentException(
-                        format("Page specifier isn't configured correctly!, begin=%d, end=%d", begin, end)
-                );
-            }
-
-            int pageSpecifierKey = url.indexOf(PAGE_SPECIFIER_KEY);
-            if (pageSpecifierKey == -1 || pageSpecifierKey < begin || pageSpecifierKey > end) {
-                throw new IllegalArgumentException(
-                        format("Page specifier is in wrong place!, begin=%d, pageKey=%d, end=%d",
-                                begin, pageSpecifierKey, end)
-                );
-            }
-
-            int hashFragmentSpecIndex = url.indexOf(HASH_FRAGMENT_SPECIFIER);
-            if (hashFragmentSpecIndex != -1) {
-                return new PageSpecifier(PageSpecifierType.HASH_FRAGMENT, hashFragmentSpecIndex, end);
-            }
-
-            int queryParamSpecIndex = url.indexOf(QUERY_PARAM_SPECIFIER);
-            if (queryParamSpecIndex != -1) {
-                return new PageSpecifier(PageSpecifierType.QUERY_PARAM, queryParamSpecIndex, end);
-            }
-
-            return new PageSpecifier(PageSpecifierType.URL_PATH, begin, end);
-        }
-
+      return new PageSpecifier(PageSpecifierType.URL_PATH, begin, end);
     }
+
+  }
 
 }
