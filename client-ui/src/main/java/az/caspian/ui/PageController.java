b1d604728eee92ac93ee0675816dffbbbc7ea264
mammadyahyayev <mammadyahyayev@gmail.com>
2024-04-22 23:17:09 +0400

    Add index page and spring boot starter web and thymeleaf dependencies

---
+++ client-ui/src/main/java/az/caspian/ui/PageController.java
@@ -0,0 +1,13 @@
+package az.caspian.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
+}
