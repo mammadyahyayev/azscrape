bc6679095289e3326d7e4c6dacf750efc87efb49
mammadyahyayev <mammadyahyayev@gmail.com>
2024-05-11 14:46:42 +0400

    Add commons-validator dependency and module

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module azscrape.core {
   requires org.apache.logging.log4j;
   requires com.google.common;
   requires org.jetbrains.annotations;
+  requires commons.validator;
 
   exports az.caspian.core;
   exports az.caspian.core.tree;
@@ -16,5 +17,5 @@ module azscrape.core {
   exports az.caspian.core.io;
   exports az.caspian.core.template;
   exports az.caspian.core.task;
-    exports az.caspian.core.tree.node;
+  exports az.caspian.core.tree.node;
 }
